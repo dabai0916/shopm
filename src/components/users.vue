6ac63a0d736ca1f1f295407f0f931587d2f26cb0
dabai0916 <793093783@qq.com>
2019-02-25 21:09:18 +0800

    '添加用户功能完成'

--- src/components/users.vue
+++ src/components/users.vue
@@ -9,14 +9,21 @@
     <!-- 搜索框 -->
     <el-row class="searchBox">
       <el-col :span="6">
-        <el-input placeholder="请输入内容" v-model="query" class="input-with-select">
-          <el-button slot="append" icon="el-icon-search"></el-button>
+        <el-input
+          @clear="getAllUsers()"
+          clearable
+          placeholder="请输入内容"
+          v-model="query"
+          class="input-with-select"
+        >
+          <el-button slot="append" icon="el-icon-search" @click="searchUser()"></el-button>
         </el-input>
       </el-col>
       <el-col :span="2">
-        <el-button type="success">添加用户</el-button>
+        <el-button type="success" @click="addUserList()">添加用户</el-button>
       </el-col>
     </el-row>
+    <!-- 表格 -->
     <el-table :data="list" stripe style="width: 100%">
       <!--
         create_time: 1486720211
@@ -27,55 +34,125 @@
         role_name: "主管"
         username: "admin"
       -->
-      <el-table-column prop="id" label="#" width="80"></el-table-column>
-      <el-table-column prop="username" label="姓名" width="100"></el-table-column>
-      <el-table-column prop="email" label="邮箱" width="140"></el-table-column>
-      <el-table-column prop="mobile" label="电话" width="140"></el-table-column>
-      <el-table-column label="创建日期" width="200">
-        <template slot-scope="list">
-          {{list.row.create_time | fmdate}}
+      <el-table-column prop="id" label="#" width="180"></el-table-column>
+      <el-table-column prop="username" label="姓名" width="200"></el-table-column>
+      <el-table-column prop="email" label="邮箱" width="220"></el-table-column>
+      <el-table-column prop="mobile" label="电话" width="220"></el-table-column>
+      <el-table-column label="创建日期" width="300">
+        <template slot-scope="scope">{{scope.row.create_time | fmdate}}</template>
+      </el-table-column>
+      <el-table-column prop="status" label="用户状态" width="200">
+        <template slot-scope="scope">
+          <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
         </template>
       </el-table-column>
-      <el-table-column prop="status" label="用户状态" width="120"></el-table-column>
-      <el-table-column prop="address" label="地址" width="200"></el-table-column>
+      <el-table-column prop="address" label="操作" width="300">
+        <el-button type="primary" icon="el-icon-edit" circle plain size="small"></el-button>
+        <el-button type="danger" icon="el-icon-delete" circle plain size="small"></el-button>
+        <el-button type="success" icon="el-icon-check" circle plain size="small"></el-button>
+      </el-table-column>
     </el-table>
+    <!-- 分页 -->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pagenum"
+      :page-sizes="[2, 4, 6, 8]"
+      :page-size="2"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+    ></el-pagination>
+    <!-- 添加用户对话框 -->
+    <el-dialog title="收货地址" :visible.sync="dialogFormVisibleAdd">
+      <el-form label-position="left" label-width="80px" :model="formdata">
+        <el-form-item label="用户名">
+          <el-input v-model="formdata.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="formdata.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱">
+          <el-input v-model="formdata.email"></el-input>
+        </el-form-item>
+        <el-form-item label="电话">
+          <el-input v-model="formdata.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleAdd = false">取 消</el-button>
+        <el-button type="primary" @click="addUser()">确 定</el-button>
+      </div>
+    </el-dialog>
   </el-card>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      query: '',
+      query: "",
       pagenum: 1,
-      pagesize: 6,
-      list: []
-    }
+      pagesize: 2,
+      total: 15,
+      list: [],
+      dialogFormVisibleAdd: false,
+      formdata: {
+        username: "",
+        password: "",
+        email: "",
+        mobile: ""
+      }
+    };
   },
-  created () {
-    this.getTableData()
+  created() {
+    this.getTableData();
   },
   methods: {
-    async getTableData () {
+    async addUser() {
+      const res = await this.$http.post(`users`, this.formdata);
+      this.dialogFormVisibleAdd = false;
+      this.getTableData();
+    },
+    addUserList() {
+      this.dialogFormVisibleAdd = true;
+    },
+    getAllUsers() {
+      this.getTableData();
+    },
+    searchUser() {
+      this.getTableData();
+    },
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+      this.pagesize = val;
+      this.getTableData();
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+      this.pagenum = val;
+      this.getTableData();
+    },
+    async getTableData() {
       // 设置请求头token值
-      const AUTH_TOKEN = localStorage.getItem('token')
-      this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN
+      const AUTH_TOKEN = localStorage.getItem("token");
+      this.$http.defaults.headers.common["Authorization"] = AUTH_TOKEN;
       const res = await this.$http.get(
         `users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${
           this.pagesize
         }`
-      )
-      console.log(res)
+      );
+      console.log(res);
       const {
         data,
         meta: { msg, status }
-      } = res.data
+      } = res.data;
       if (status === 200) {
-        this.list = data.users
+        this.total = data.total;
+        this.list = data.users;
       }
     }
   }
-}
+};
 </script>
 
 <style>
